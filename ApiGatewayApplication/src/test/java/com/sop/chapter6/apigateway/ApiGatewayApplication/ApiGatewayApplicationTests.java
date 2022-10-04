90c817affddfdd17a31a2b6debfee7a5db0644f4
63070188 <68292431+63070188@users.noreply.github.com>
2022-10-04 10:24:00 +0700

    Add files via upload

---
+++ ApiGatewayApplication/src/test/java/com/sop/chapter6/apigateway/ApiGatewayApplication/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sop.chapter6.apigateway.ApiGatewayApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
